d52a1a1653a8c36f5578f3bc69fca995930ac837
RubenSouza <rubem.eliel20212@gmail.com>
2023-10-15 20:54:56 -0300

    tab bar icons

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Home from "./pages/Home";
+import { House, List, Bell } from "phosphor-react-native";
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -9,18 +9,30 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Menu" component={Home} />
+        <Tab.Screen
+          name="Menu"
+          component={Home}
+          options={{
+            tabBarLabel: "Menu",
+            tabBarIcon: ({ color }) => <List color={color} size={26} />,
+          }}
+        />
         <Tab.Screen
           name="Home"
           component={Home}
           options={{
             tabBarLabel: "Home",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
-            ),
+            tabBarIcon: ({ color }) => <House color={color} size={26} />,
           }}
         />
-        <Tab.Screen name="Notificações" component={Home} />
+        <Tab.Screen
+          name="Notificações"
+          component={Home}
+          options={{
+            tabBarLabel: "Notificações",
+            tabBarIcon: ({ color }) => <Bell color={color} size={26} />,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
