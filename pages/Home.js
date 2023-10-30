508e97a7a5bce69fa0e1ba5b090029313dc0ec26
RubenSouza <rubem.eliel20212@gmail.com>
2023-10-30 11:27:18 -0300

    month divisor in the schedule component

--- pages/Home.js
+++ pages/Home.js
@@ -1,16 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Button } from "react-native-paper";
+import lista from "../libs/lista.json";
 
 export default function Home() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Home</Text>
-      <Button
-        icon="details"
-        mode="outlined"
-        onPress={() => console.log("Pressed")}
-      >
+      <Button icon="details" mode="outlined" onPress={() => console.log(lista)}>
         Press me
       </Button>
     </View>
--- pages/Schedule.js
+++ pages/Schedule.js
@@ -1,19 +1,100 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import lista from "../libs/lista.json";
+import moment from "moment";
+
+const data = new Date("2023-10-30");
+
+moment.updateLocale("pt-br", {
+  months:
+    "janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro".split(
+      "_"
+    ),
+  monthsShort: "jan_fev_mar_abr_mai_jun_jul_ago_set_out_nov_dez".split("_"),
+});
+
+moment.locale("pt-br");
+
+let aulas = lista;
+
+let dataAtual = new Date();
+
+let aulasPassadas = [];
+let aulasFuturas = [];
+
+aulasFuturas.sort((a, b) => new Date(a.data) - new Date(b.data));
+aulasPassadas.sort((a, b) => new Date(a.data) - new Date(b.data));
+
+for (let i = 0; i < aulas.length; i++) {
+  let dataAula = new Date(aulas[i].data);
+
+  if (dataAula <= dataAtual) {
+    aulasPassadas.push(aulas[i]);
+  } else {
+    aulasFuturas.push(aulas[i]);
+  }
+}
+
+const aulasPassadasPorMesAno = {};
+const aulasFuturasPorMesAno = {};
+
+for (let i = 0; i < aulasPassadas.length; i++) {
+  const dataAula = new Date(aulasPassadas[i].data);
+  const mesAno = moment(dataAula).format("MMMM YYYY");
+
+  if (!aulasPassadasPorMesAno[mesAno]) {
+    aulasPassadasPorMesAno[mesAno] = [];
+  }
+
+  aulasPassadasPorMesAno[mesAno].push(aulasPassadas[i]);
+}
+
+for (let i = 0; i < aulasFuturas.length; i++) {
+  const dataAula = new Date(aulasFuturas[i].data);
+  const mesAno = moment(dataAula).format("MMMM YYYY");
+
+  if (!aulasFuturasPorMesAno[mesAno]) {
+    aulasFuturasPorMesAno[mesAno] = [];
+  }
+
+  aulasFuturasPorMesAno[mesAno].push(aulasFuturas[i]);
+}
+
+const MonthList = ({ data }) => {
+  return (
+    <FlatList
+      data={data}
+      renderItem={({ item }) => {
+        const [mes, ano] = item[0].split(" ");
+        return (
+          <View>
+            <Text className="capitalize m-auto text-slate-400 text-sm">{`${mes} de ${ano}`}</Text>
+            {item[1].map((aula, i) => (
+              <View key={i}>
+                <Text>{aula?.data}</Text>
+                <Text>{aula?.nome}</Text>
+              </View>
+            ))}
+          </View>
+        );
+      }}
+    />
+  );
+};
 
 const NextClasses = () => {
   return (
-    <View>
-      <Text>Schedule</Text>
+    <View className="my-2">
+      <MonthList data={Object.entries(aulasFuturasPorMesAno)} />
     </View>
   );
 };
 
 const PreviousClasses = () => {
   return (
-    <View>
-      <Text>Schedule2</Text>
+    <View className="my-2">
+      <MonthList data={Object.entries(aulasPassadasPorMesAno)} />
     </View>
   );
 };
